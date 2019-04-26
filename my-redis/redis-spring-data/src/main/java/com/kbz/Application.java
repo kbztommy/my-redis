363108a8d168cf59614401439420672fb119e4bc
Tommy <kbztommy@gmail.com>
2019-04-26 10:07:45 +0800

    增加redis lettuce connection pool的設定 修改key的Serializer為StringRedisSerializer

--- my-redis/redis-spring-data/src/main/java/com/kbz/Application.java
+++ my-redis/redis-spring-data/src/main/java/com/kbz/Application.java
@@ -1,3 +1,4 @@
+
 package com.kbz;
 
 import org.springframework.boot.SpringApplication;
--- my-redis/redis-spring-data/src/main/java/com/kbz/config/RedisConfig.java
+++ my-redis/redis-spring-data/src/main/java/com/kbz/config/RedisConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,15 +17,20 @@ public class RedisConfig {
   @Bean
   public RedisTemplate<String, Object> redisTemplate(
       RedisConnectionFactory redisConnectionFactory) {
-    RedisTemplate<String, Object> redisTemplate;
-    redisTemplate = new RedisTemplate<String, Object>();
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+    ;
+
+    RedisSerializer<String> stringRedisSerializer = new StringRedisSerializer();
+
     Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
         new Jackson2JsonRedisSerializer<>(Object.class);
     ObjectMapper om = new ObjectMapper();
     om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
     om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
     jackson2JsonRedisSerializer.setObjectMapper(om);
     redisTemplate.setConnectionFactory(redisConnectionFactory);
+    redisTemplate.setKeySerializer(stringRedisSerializer);
     redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
     redisTemplate.afterPropertiesSet();
     return redisTemplate;
